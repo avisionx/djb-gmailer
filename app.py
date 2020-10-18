433691420d5626ffe29ac0977f9d67696393f188
avisionx <avi.grg4567@gmail.com>
2020-10-19 00:46:56 +0530

    env fixes

--- app.py
+++ app.py
@@ -16,19 +16,17 @@ with open(COMPLAINTS_FILE, 'a+') as COMPLAINTS_FILE_OBJ:
 # NOTE: ALLOW LESS SECURE APPS IN GMAIL
 SECRET_EMAIL = os.getenv("SECRET_EMAIL")
 SECRET_PWD = os.getenv("SECRET_PWD")
-BASIC_AUTH_REALM = os.getenv("BASIC_AUTH_REALM")
 BASIC_AUTH_USERNAME = os.getenv("BASIC_AUTH_USERNAME")
 BASIC_AUTH_PASSWORD = os.getenv("BASIC_AUTH_PASSWORD")
 
 app = Flask(__name__)
 
-app.config['BASIC_AUTH_REALM'] = BASIC_AUTH_REALM
 app.config['BASIC_AUTH_USERNAME'] = BASIC_AUTH_USERNAME
 app.config['BASIC_AUTH_PASSWORD'] = BASIC_AUTH_PASSWORD
 
 basic_auth = BasicAuth(app)
 
-# Create a cron job like so crontab -e add this
+# Create a cron job like so crontab -e add this username and password from .env
 # * * * * * /opt/local/bin/curl -X GET https://falken:joshua@YOUR_APP/cron/do_the_thing
 @app.route('/cron/email', methods=['GET'])
 @basic_auth.required
